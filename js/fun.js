3a79d7979f6dc72bfc979a653ba39e7afb5edbba
Stephanie He <he.stephanie.y@gmail.com>
2016-11-22 14:25:03 +0800

    Easter eggs

---
+++ js/fun.js
@@ -0,0 +1,5 @@
+// Console output
+console.log("Hello, developer friend!");
+console.log("This website is made with love and its source is available at https://github.com/StephanieYHe/StephanieYHe.github.io!")
+console.log("See any bugs? Feel free to send me an email at he.stephanie.y+site@gmail.com.");
+console.log("Love, Stephanie")
