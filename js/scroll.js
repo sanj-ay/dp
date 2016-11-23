5d758ceebc491bd353cebc904487b00d69124300
Stephanie He <he.stephanie.y@gmail.com>
2016-11-23 09:52:49 +0800

    Pin navigation bar when scrolling, using ScrollMagic

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// Pin nav bar to the top
+new ScrollMagic.Scene({
+        offset: 100
+    })
+    .setPin("#site-navigation")
+    .setClassToggle("#site-navigation", "site-navigation-pinned")
+    .addTo(controller);
