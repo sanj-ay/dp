4756b484707584d660c7d6b52d44cba3a922d771
Stephanie He <he.stephanie.y@gmail.com>
2016-11-23 11:00:25 +0800

    Smooth scrolling for pinned nav bar

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ var controller = new ScrollMagic.Controller();
 
 // Pin nav bar to the top
 new ScrollMagic.Scene({
-        offset: 100
+        offset: 107
     })
     .setPin("#site-navigation")
     .setClassToggle("#site-navigation", "site-navigation-pinned")
