cd95f7534b900781ba1c7fa6999ca3a8a6627cb7
Stephanie He <he.stephanie.y@gmail.com>
2016-12-01 03:32:49 -0800

    Include mouseflow analytics code

---
+++ js/mouseflow.js
@@ -0,0 +1,7 @@
+var _mfq = _mfq || [];
+(function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.async = true;
+    mf.src = "//cdn.mouseflow.com/projects/1d9fc9a6-4809-4774-8309-488cc5da636c.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})();
