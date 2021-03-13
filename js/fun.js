29139f17180c3c6a4fdffb05a007d274f589ab8a
Sanjay K <sanjay.work@hotmail.com>
2021-03-13 16:02:27 +0530

    Removed files

--- js/fun.js
+++
@@ -1,5 +0,0 @@
-// Console output
-console.log("Hello, developer friend!");
-console.log("This website is made with love and its source is available at https://github.com/StephanieYHe/StephanieYHe.github.io!")
-console.log("See any bugs? Feel free to open an issue on the repo!");
-console.log("Love, Stephanie")
